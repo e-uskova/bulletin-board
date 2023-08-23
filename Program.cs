b2373576ca3d400d75bdeee3f451965f1e44c247
Ekaterina <60484474+e-uskova@users.noreply.github.com>
2023-08-23 21:02:37 +0300

    Создана структура приложения

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace bulletin_board
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
